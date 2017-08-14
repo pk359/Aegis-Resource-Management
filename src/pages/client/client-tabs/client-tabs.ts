d047783b8e0112c3c2a9e9dd7ce6870f71269b4b
Abyssaliz <qisheng_50@hotmail.com>
2017-08-14 22:48:34 +0800

    complete slide menu for manager, tradesperson, client, engineer and menu for the feedback page, new order page, new user page and service list page

--- src/pages/client/client-tabs/client-tabs.ts
+++ src/pages/client/client-tabs/client-tabs.ts
@@ -1,16 +1,34 @@
+import { NewOrderPage } from './../new-order-page/new-order-page';
 import { FeedbackPage } from './../../feedback/feedback-page/feedback-page';
 import { CurrentJobsPage } from './../../common/current-jobs-page/current-jobs-page';
-import { Component } from '@angular/core';
-import { NewOrderPage } from '../new-order-page/new-order-page'
-
+import { Component, ViewChild } from '@angular/core';
+import { Nav } from 'ionic-angular'
+import * as firebase from 'firebase'
+import { UserHelper } from './../../common/Utilities/user-helper';
+import { NavController} from 'ionic-angular';
+import { LoginPage } from "../../common/login-page/login-page";
 @Component({
   templateUrl: 'client-tabs.html',
 })
 export class ClientTabs {
 
-  tab1Root = NewOrderPage;
-  tab2Root = CurrentJobsPage;
-  tab3Root = FeedbackPage;
-  constructor() {
+  @ViewChild(Nav) nav: Nav;
+  pages: Array<{ title: string, icon: string, component: any }>;
+  rootPage: any = CurrentJobsPage;
+  constructor(public navCtrl: NavController) {
+    this.pages = [
+      {title: 'Current Jobs', icon:'logo-buffer', component: CurrentJobsPage},
+      {title: 'New Order', icon:'ios-clipboard', component: NewOrderPage},
+      {title: 'Feedback', icon: 'ios-paper', component: FeedbackPage}
+    ];
+  }
+  openPage(page){
+    this.nav.setRoot(page.component);
+  }
+  logout() {
+    firebase.auth().signOut().then(_=>{
+      this.navCtrl.push(LoginPage);
+      UserHelper.removeUser();
+    })
   }
 }
--- src/pages/tradesperson/trades-person-tabs/trades-person-tabs.ts
+++ src/pages/tradesperson/trades-person-tabs/trades-person-tabs.ts
@@ -1,6 +1,11 @@
 import { FeedbackPage } from './../../feedback/feedback-page/feedback-page';
 import { CurrentJobsPage } from './../../common/current-jobs-page/current-jobs-page';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Nav } from 'ionic-angular'
+import * as firebase from 'firebase'
+import { UserHelper } from './../../common/Utilities/user-helper';
+import { NavController} from 'ionic-angular';
+import { LoginPage } from "../../common/login-page/login-page";
 
 
 @Component({
@@ -8,9 +13,22 @@ import { Component } from '@angular/core';
 })
 export class TradesPersonTabs {
 
-  tab2Root = CurrentJobsPage
-  tab3Root = FeedbackPage;
-  constructor() {
+  @ViewChild(Nav) nav: Nav;
+  pages: Array<{ title: string, icon: string, component: any }>;
+  rootPage: any = CurrentJobsPage;
+  constructor(public navCtrl: NavController) {
+    this.pages = [
+      {title: 'Current Jobs', icon:'logo-buffer', component: CurrentJobsPage},
+      {title: 'Feedback', icon: 'ios-paper', component: FeedbackPage}
+    ];
   }
-
+  openPage(page){
+    this.nav.setRoot(page.component);
+  }
+  logout() {
+    firebase.auth().signOut().then(_=>{
+      this.navCtrl.push(LoginPage);
+      UserHelper.removeUser();
+    })
+  }
 }
